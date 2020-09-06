1bc3d683cb4e6fff3388e0d52472c50623da541f
藤田竜貴 <rinsanity@ryuki-fujita.xyz>
2020-09-06 19:16:00 +0900

    block.py 仮

---
+++ src/block.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Block(Enum) :
+    ## 旧版
+    # PON = 1
+    # CHI = 2
+    # ANKAN = 3
+    # MINKAN = 4
+    # ANKO = 5
+    # SYUNTSU = 6
+    # TOITSU = 7
+
+    OPENED_RUN = 1        # チー
+    CLOSED_RUN = 2        # 順子
+    OPENED_TRIPLETS = 3   # ポン
+    CLOSED_TRIPLETS = 4   # 暗刻
+    OPENED_KAN = 5        # 明槓（大明槓, 加槓）
+    CLOSED_KAN = 6        # 暗槓
+    PAIR = 7              # 対子
+
