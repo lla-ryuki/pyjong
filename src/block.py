4aac57e2fa2d82d1a9fbabc62d4e374e8414dd6f
藤田竜貴 <rinsanity@ryuki-fujita.xyz>
2020-09-06 19:24:14 +0900

    変数名修正

--- src/block.py
+++ src/block.py
@@ -12,8 +12,8 @@ class Block(Enum) :
 
     OPENED_RUN = 1        # チー
     CLOSED_RUN = 2        # 順子
-    OPENED_TRIPLETS = 3   # ポン
-    CLOSED_TRIPLETS = 4   # 暗刻
+    OPENED_TRIPLET = 3    # ポン
+    CLOSED_TRIPLET = 4    # 暗刻
     OPENED_KAN = 5        # 明槓（大明槓, 加槓）
     CLOSED_KAN = 6        # 暗槓
     PAIR = 7              # 対子
