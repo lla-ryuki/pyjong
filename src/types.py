5088f51993b9be7f4caac3ff9902b347fdd450b9
lla-ryuki <account@ryuki-fujita.xyz>
2020-10-27 22:43:12 +0900

    typesで定義しているsetを変更不可にするためfrozensetで定義

--- src/types.py
+++ src/types.py
@@ -4,15 +4,15 @@ class ConstMeta(type):
         else: self.__setattr__(name, value)
 
 class TileType(metaclass=ConstMeta) :
-    REDS      = {0, 10, 20}                   # 赤の牌番号（0:赤5萬, 10, 赤5筒, 20:赤5索）
-    BLANKS    = {0, 10, 20, 30}               # handで使わない番号
-    FIVES     = {5, 15, 25}                   # 各種5の牌の牌番号
-    NINES     = {9, 19, 29}                   # 各種9の牌の牌番号
-    GREENS    = {22, 23, 24, 26, 28, 36}      # 索子の緑 緑一色用
-    TERMINALS = {1, 11, 21, 9, 19, 29}        # 端牌
-    WINDS     = {31, 32, 33, 34}              # 風牌
-    DRAGONS   = {35, 36, 37}                  # 白, 發, 中
-    HONORS    = {31, 32, 33, 34, 35, 36, 37}  # 字牌
+    REDS      = frozenset({0, 10, 20})                   # 赤の牌番号（0:赤5萬, 10, 赤5筒, 20:赤5索）
+    BLANKS    = frozenset({0, 10, 20, 30})               # handで使わない番号
+    FIVES     = frozenset({5, 15, 25})                   # 各種5の牌の牌番号
+    NINES     = frozenset({9, 19, 29})                   # 各種9の牌の牌番号
+    GREENS    = frozenset({22, 23, 24, 26, 28, 36})      # 索子の緑 緑一色用
+    TERMINALS = frozenset({1, 11, 21, 9, 19, 29})        # 端牌
+    WINDS     = frozenset({31, 32, 33, 34})              # 風牌
+    DRAGONS   = frozenset({35, 36, 37})                  # 白, 發, 中
+    HONORS    = frozenset({31, 32, 33, 34, 35, 36, 37})  # 字牌
 
 class BlockType(metaclass=ConstMeta) :
     OPENED_RUN = 1        # チー
@@ -23,7 +23,7 @@ class BlockType(metaclass=ConstMeta) :
     CLOSED_KAN = 6        # 暗槓
     PAIR = 7              # 対子
 
-    RUNS      = {OPENED_RUN, CLOSED_RUN}
-    TRIPLETS  = {OPENED_TRIPLET, CLOSED_TRIPLET}
-    KANS      = {OPENED_KAN, CLOSED_KAN}
+    RUNS      = frozenset({OPENED_RUN, CLOSED_RUN})
+    TRIPLETS  = frozenset({OPENED_TRIPLET, CLOSED_TRIPLET})
+    KANS      = frozenset({OPENED_KAN, CLOSED_KAN})
 
