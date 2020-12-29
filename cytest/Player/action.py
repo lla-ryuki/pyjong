c93a5c32e2878f0881c772cfe67ad803f2a7f517
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-12-29 19:21:46 +0900

    add: Playerモジュール追加，Dahaiタグを処理するメソッド実装

---
+++ cytest/Player/action.py
@@ -0,0 +1,30 @@
+# sys
+from typing import List
+
+# ours
+from mytypes import TileType
+
+
+class Action :
+    def __init__(self) :
+        self.reset_N()
+
+
+    # 切る牌を決める
+    def decide_which_tile_to_discard(self, game, players, player_num) -> (int, bool) :
+        # エラーチェック
+        if game.tag_name[0] not in {"D", "E", "F", "G"} : game.error("Wrong tag (in Player.action.decide_which_tile_to_discard()")
+        if   game.tag_name[0] == "D" : i_player = 0
+        elif game.tag_name[0] == "E" : i_player = 1
+        elif game.tag_name[0] == "F" : i_player = 2
+        elif game.tag_name[0] == "G" : i_player = 3
+        if i_player != player_num : game.error("Player index don't match (in decide_which_tile_to_discard())")
+
+        tile = int(game.tag_name[1:])
+        discarded_tile = game.convert_tile(tile)
+        exchanged = False
+        if tile != game.org_got_tile : exchanged = True
+
+        game.read_next_tag()
+        return discarded_tile, exchanged
+
