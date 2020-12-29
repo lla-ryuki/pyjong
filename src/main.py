d2f29ff54b61d0eb3ac5f8e6017bc81917e99d77
neuzen-ryuki <account@ryuki-fujita.xyz>
2020-12-29 19:34:40 +0900

    update: main.py プレイさせたいゲーム数を標準入力で決定できるように変更

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), './cymod'))
 
 # 3rd
+from termcolor import colored
 
 # ours
 from game import Game
@@ -11,7 +12,8 @@ from Player.action import Action
 
 
 if __name__ == "__main__" :
+    games_num = int(input(colored("Input games num: ","yellow", attrs=["bold"])))
+
     action = Action()
-    # ゲーム処理
     game = Game(action=action)
-    game.play_game()
+    game.play_games(games_num)
